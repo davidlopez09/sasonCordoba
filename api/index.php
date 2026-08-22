6a8ee4a34e32d325fedb0b49ca5ad5cf8f0aea6c
JoseDiaz18 <joseangel191134@gmail.com>
2026-08-22 12:59:39 -0500

    fix(cors): Habilitar metodos PUT, DELETE y cabecera Authorization en CORS para permitir consumo de API desde frontends desacoplados

--- api/index.php
+++ api/index.php
@@ -7,8 +7,8 @@ $corsOrigin = $config['cors']['origin'] ?? '*';
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: ' . $corsOrigin);
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
